9a9a54a1f1763ab863057f7786c8072ce4a16922
Huy Huỳnh Nguyễn Minh <dev.hnmhuy@gmail.com>
2024-09-26 21:06:17 +0700

    update: CORS and env config

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const port = process.env.PORT || 4000
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const allowedOrigins = configService.get('ALLOWED_ORIGINS');
 
    // Enable CORS globally
-   app.enableCors({
-    origin: ['https://golden-owl-weather-app-ui.vercel.app', 'http://localhost:3000'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: false,
-  });
+    app.enableCors({
+      origin: allowedOrigins.split(','),
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+    });
 
   await app.listen(port);
 }
